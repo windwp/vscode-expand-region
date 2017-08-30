bd5ccaedb2e5cd4aab595b3e9d07ea35967cc13d
mrtrieu <longtrieu1912@gmail.com>
2017-08-30 10:00:20 +0700

    [Feature]  support mutli cursor

--- src/expandermanager.ts
+++ src/expandermanager.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
-import {BaseExpander, LanguageType, } from './baseexpander'
+import { BaseExpander, LanguageType, } from './baseexpander'
 import * as expander from './_expander';
 export class ExpanderManager {
-    expandHistory: Array<{ start: vscode.Position, end: vscode.Position, resultStart: vscode.Position, resultEnd: vscode.Position }> = [];
+    expandHistory: Array<Array<{ start: vscode.Position, end: vscode.Position, resultStart: vscode.Position, resultEnd: vscode.Position, index: number }>> = [];
     expand() {
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -20,33 +20,55 @@ export class ExpanderManager {
                     break;
             }
         }
+        console.log("lala");
         let text = doc.getText();
-        let start = doc.offsetAt(editor.selection.start);
-        let end = doc.offsetAt(editor.selection.end);
-        let result = exp.expand(text, start, end);
-        if (result) {
-            let startPos = doc.positionAt(result.end);
-            let endPos = doc.positionAt(result.start);
-            if (this.expandHistory.length > 0) {
-                let historyPosition = this.expandHistory[this.expandHistory.length - 1];
-                if (historyPosition.resultStart.compareTo(editor.selection.start) !== 0 || historyPosition.resultEnd.compareTo(editor.selection.end) !== 0) {
-                    //move to new expand
-                    this.expandHistory = [];
+        //multiple selection
+        editor.selections = editor.selections.map((iSelection, selectionIdx) => {
+            try {
+                console.log('add position ' + selectionIdx);
+                let start = doc.offsetAt(iSelection.start);
+                let end = doc.offsetAt(iSelection.end);
+                let result = exp.expand(text, start, end);
+                if (result) {
+                    let startPos = doc.positionAt(result.end);
+                    let endPos = doc.positionAt(result.start);
+                    if (this.expandHistory[selectionIdx] && this.expandHistory[selectionIdx].length > 0) {
+                        let historyPosition = this.expandHistory[selectionIdx][this.expandHistory[selectionIdx].length - 1];
+                        if (historyPosition.resultStart.compareTo(iSelection.start) !== 0 || historyPosition.resultEnd.compareTo(iSelection.end) !== 0) {
+                            //move to new expand delete all history position of index
+                            console.log('remove all history position ' + selectionIdx);
+                            this.expandHistory[selectionIdx] = [];
+                        }
+                    }
+                    if (!this.expandHistory[selectionIdx]) this.expandHistory[selectionIdx] = [];
+                    this.expandHistory[selectionIdx].push({ start: iSelection.start, end: iSelection.end, resultEnd: endPos, resultStart: startPos, index: selectionIdx });
+                    iSelection = new vscode.Selection(startPos, endPos)
+                    // editor.selection = newselection;
                 }
+            } catch (error) {
+                console.log(error);
             }
-            this.expandHistory.push({ start: editor.selection.start, end: editor.selection.end, resultEnd: endPos, resultStart: startPos });
-            let newselection = new vscode.Selection(startPos, endPos)
-            editor.selection = newselection;
-        }
+            console.log(selectionIdx);
+            return iSelection;
+        })
+
     }
 
     undoExpand() {
         if (this.expandHistory.length > 0) {
             // console.log("undoExpand")
             let editor = vscode.window.activeTextEditor;
-            let historyPosition = this.expandHistory.pop();
-            console.dir(historyPosition)
-            editor.selection = new vscode.Selection(historyPosition.start, historyPosition.end)
+
+            // console.dir(historyPosition)
+            editor.selections = editor.selections.map((iSelection, selectionIdx) => {
+                if (this.expandHistory[selectionIdx].length > 0) {
+                    let historyPosition = this.expandHistory[selectionIdx].pop();
+                    if (historyPosition) {
+                        iSelection = new vscode.Selection(historyPosition.start, historyPosition.end);
+                    }
+                }
+                return iSelection;
+            })
         }
     }
 
